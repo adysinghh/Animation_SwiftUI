4ec469d84ce54946ddaba75cf3825526e9400d2f
adysinghh <aditya12345rajput56@gmail.com>
2024-12-02 17:31:33 +0530

    Initial Commit

---
+++ Animation_SwiftUI/Animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SwiftUIApp.swift
+//  Animation_SwiftUI
+//
+//  Created by Aditya Singh on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation_SwiftUITests/Animation_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SwiftUITests.swift
+//  Animation_SwiftUITests
+//
+//  Created by Aditya Singh on 02/12/24.
+//
+
+import Testing
+@testable import Animation_SwiftUI
+
+struct Animation_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
